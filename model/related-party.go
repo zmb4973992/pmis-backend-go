03c182d5aa130b61fa49591e715069bc09503aed
zmb <19725912@qq.com>
2023-02-06 16:22:59 +0800

    revised in office

--- model/related-party.go
+++ model/related-party.go
@@ -2,12 +2,12 @@ package model
 
 type RelatedParty struct {
 	BaseModel
-	ChineseName             *string `json:"chinese_name" binding:"required"`               //中文名称
-	EnglishName             *string `json:"english_name" binding:"required"`               //英文名称
-	Address                 *string `json:"address" binding:"required"`                    //地址
-	UniformSocialCreditCode *string `json:"uniform_social_credit_code" binding:"required"` //统一社会信用代码
-	Telephone               *string `json:"telephone" binding:"required"`                  //电话
-	//外键
+	ChineseName             *string `json:"chinese_name"`               //中文名称
+	EnglishName             *string `json:"english_name"`               //英文名称
+	Address                 *string `json:"address"`                    //地址
+	UniformSocialCreditCode *string `json:"uniform_social_credit_code"` //统一社会信用代码
+	Telephone               *string `json:"telephone"`                  //电话
+	Remarks                 *string `json:"remarks"`                    //备注
 }
 
 // TableName 修改数据库的表名
