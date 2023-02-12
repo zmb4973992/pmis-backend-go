a0a5713df4501c4671e1efb2e47e869c637da2ed
zmb <19725912@qq.com>
2023-02-12 09:46:45 +0800

    revised at home

--- model/role.go
+++ model/role.go
@@ -8,8 +8,9 @@ import (
 
 type Role struct {
 	BaseModel
-	Name     string //角色名称
-	Sequence int    //顺序值，权限越大的值越大，用来比较用的
+	Name     string  //角色名称
+	Sequence int     //顺序值，权限越大的值越大，用来比较用的
+	User     []*User `gorm:"many2many:user_role"`
 	//这里是声名外键关系，并不是实际字段。不建议用gorm的多对多的设定，不好修改
 	//User []RoleAndUser `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
