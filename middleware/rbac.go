30a208ffb5814f7d073ab7ab87b19e46669e7c60
zmb <19725912@qq.com>
2023-05-03 22:52:37 +0800

    revised at home

--- middleware/rbac.go
+++ middleware/rbac.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"pmis-backend-go/global"
-	"pmis-backend-go/model"
 	"pmis-backend-go/serializer/response"
 	"pmis-backend-go/util"
 	"strconv"
@@ -19,10 +18,7 @@ func RBAC() gin.HandlerFunc {
 			return
 		}
 
-		var roleIDs []int
-		global.DB.Model(&model.UserAndRole{}).Where("user_id = ?", userID).
-			Select("role_id").Find(&roleIDs)
-
+		subject := strconv.Itoa(userID)
 		object := c.Request.URL.Path //获取请求路径，casbin规则的客体参数
 		act := c.Request.Method      //获取请求方法，casbin规则的动作参数
 
@@ -31,14 +27,7 @@ func RBAC() gin.HandlerFunc {
 			global.SugaredLogger.Errorln(err)
 		}
 
-		var permitted bool
-		for _, role := range roleIDs {
-			subject := strconv.Itoa(role)
-			permitted, _ = cachedEnforcer.Enforce(subject, object, act)
-			if permitted {
-				break
-			}
-		}
+		permitted, _ := cachedEnforcer.Enforce(subject, object, act)
 
 		if !permitted {
 			c.AbortWithStatusJSON(http.StatusOK, response.Failure(util.ErrorUnauthorized))
--- service/user-and-role.go
+++ service/user-and-role.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"errors"
+	"gorm.io/gorm"
 	"pmis-backend-go/global"
 	"pmis-backend-go/model"
 	"pmis-backend-go/serializer/response"
@@ -8,10 +10,16 @@ import (
 	"strconv"
 )
 
-type roleAndUser struct{}
-
 //以下为入参
 //有些字段不用json tag，因为不从前端读取，而是在controller中处理
+
+var (
+	ErrorFailToDeleteRecord               = errors.New(util.GetMessage(util.ErrorFailToDeleteRecord))
+	ErrorFailToCreateRecord               = errors.New(util.GetMessage(util.ErrorFailToCreateRecord))
+	ErrorFailToUpdateRecord               = errors.New(util.GetMessage(util.ErrorFailToUpdateRecord))
+	ErrorFieldsToBeCreatedNotFound        = errors.New(util.GetMessage(util.ErrorFieldsToBeCreatedNotFound))
+	ErrorFailToUpdateRBACGroupingPolicies = errors.New(util.GetMessage(util.ErrorFailToUpdateRBACGroupingPolicies))
+)
 
 type RoleAndUserUpdateByRoleID struct {
 	Creator      int
@@ -29,117 +37,152 @@ type RoleAndUserUpdateByUserID struct {
 	RoleIDs []int `json:"role_ids,omitempty"`
 }
 
-func (r1 *RoleAndUserUpdateByRoleID) Update() response.Common {
-	//先删掉原始记录
-	err := global.DB.Where("role_id = ?", r1.RoleID).Delete(&model.UserAndRole{}).Error
-	if err != nil {
-		global.SugaredLogger.Errorln(err)
-		return response.Failure(util.ErrorFailToDeleteRecord)
-	}
-
-	//再增加新的记录
-	var paramOut []model.UserAndRole
-	for _, userID := range r1.UserIDs {
-		var record model.UserAndRole
-		if r1.Creator > 0 {
-			record.Creator = &r1.Creator
-		}
-		if r1.LastModifier > 0 {
-			record.LastModifier = &r1.LastModifier
-		}
-
-		record.RoleID = r1.RoleID
-		record.UserID = userID
-		paramOut = append(paramOut, record)
-	}
-
-	for i := range paramOut {
-		//计算有修改值的字段数，分别进行不同处理
-		tempParamOut, err := util.StructToMap(paramOut[i])
+func (r *RoleAndUserUpdateByRoleID) Update() response.Common {
+	err := global.DB.Transaction(func(tx *gorm.DB) error {
+		//先删掉原始记录
+		err := tx.Where("role_id = ?", r.RoleID).Delete(&model.UserAndRole{}).Error
 		if err != nil {
-			return response.Failure(util.ErrorFailToUpdateRecord)
+			global.SugaredLogger.Errorln(err)
+			return ErrorFailToDeleteRecord
 		}
-		paramOutForCounting := util.MapCopy(tempParamOut,
-			"Creator", "LastModifier", "CreateAt", "UpdatedAt")
 
-		if len(paramOutForCounting) == 0 {
-			return response.Failure(util.ErrorFieldsToBeCreatedNotFound)
+		//再增加新的记录
+		var paramOut []model.UserAndRole
+		for _, userID := range r.UserIDs {
+			var record model.UserAndRole
+			if r.Creator > 0 {
+				record.Creator = &r.Creator
+			}
+			if r.LastModifier > 0 {
+				record.LastModifier = &r.LastModifier
+			}
+
+			record.RoleID = r.RoleID
+			record.UserID = userID
+			paramOut = append(paramOut, record)
 		}
-	}
 
-	err = global.DB.Create(&paramOut).Error
-	if err != nil {
-		global.SugaredLogger.Errorln(err)
+		for i := range paramOut {
+			//计算有修改值的字段数，分别进行不同处理
+			tempParamOut, err := util.StructToMap(paramOut[i])
+			if err != nil {
+				return ErrorFailToUpdateRecord
+			}
+			paramOutForCounting := util.MapCopy(tempParamOut,
+				"Creator", "LastModifier", "CreateAt", "UpdatedAt")
+
+			if len(paramOutForCounting) == 0 {
+				return ErrorFieldsToBeCreatedNotFound
+			}
+		}
+
+		err = global.DB.Create(&paramOut).Error
+		if err != nil {
+			global.SugaredLogger.Errorln(err)
+			return ErrorFailToCreateRecord
+		}
+
+		//更新casbin的rbac分组规则
+		var param1 rbacUpdateGroupingPolicyByGroup
+		param1.Group = strconv.Itoa(r.RoleID)
+		for _, userID := range r.UserIDs {
+			param1.Members = append(param1.Members, strconv.Itoa(userID))
+		}
+		err = param1.Update()
+		if err != nil {
+			return ErrorFailToUpdateRBACGroupingPolicies
+		}
+
+		return nil
+	})
+
+	switch err {
+	case nil:
+		return response.Success()
+	case ErrorFailToCreateRecord:
 		return response.Failure(util.ErrorFailToCreateRecord)
-	}
-
-	//更新casbin的rbac分组规则
-	var param1 rbacUpdateGroupingPolicyByFather
-	param1.Father = strconv.Itoa(r1.RoleID)
-	for _, userID := range r1.UserIDs {
-		param1.Sons = append(param1.Sons, strconv.Itoa(userID))
-	}
-	err = param1.UpdateGroupingPolicyByFather()
-	if err != nil {
+	case ErrorFailToDeleteRecord:
+		return response.Failure(util.ErrorFailToDeleteRecord)
+	case ErrorFieldsToBeCreatedNotFound:
+		return response.Failure(util.ErrorFieldsToBeCreatedNotFound)
+	case ErrorFailToUpdateRBACGroupingPolicies:
 		return response.Failure(util.ErrorFailToUpdateRBACGroupingPolicies)
+	default:
+		return response.Failure(util.ErrorFailToUpdateRecord)
 	}
-
-	return response.Success()
 }
 
-func (r2 *RoleAndUserUpdateByUserID) Update() response.Common {
-	//先删掉原始记录
-	err := global.DB.Where("user_id = ?", r2.UserID).Delete(&model.UserAndRole{}).Error
-	if err != nil {
-		global.SugaredLogger.Errorln(err)
-		return response.Failure(util.ErrorFailToUpdateRecord)
-	}
-
-	//再增加新的记录
-	var paramOut []model.UserAndRole
-	for _, roleID := range r2.RoleIDs {
-		var record model.UserAndRole
-		if r2.Creator > 0 {
-			record.Creator = &r2.Creator
-		}
-		if r2.LastModifier > 0 {
-			record.LastModifier = &r2.LastModifier
-		}
-
-		record.UserID = r2.UserID
-		record.RoleID = roleID
-		paramOut = append(paramOut, record)
-	}
-
-	for i := range paramOut {
-		//计算有修改值的字段数，分别进行不同处理
-		tempParamOut, err := util.StructToMap(paramOut[i])
+func (r *RoleAndUserUpdateByUserID) Update() response.Common {
+	err := global.DB.Transaction(func(tx *gorm.DB) error {
+		//先删掉原始记录
+		err := tx.Where("user_id = ?", r.UserID).Delete(&model.UserAndRole{}).Error
 		if err != nil {
-			return response.Failure(util.ErrorFailToUpdateRecord)
+			global.SugaredLogger.Errorln(err)
+			return ErrorFailToDeleteRecord
 		}
-		paramOutForCounting := util.MapCopy(tempParamOut,
-			"Creator", "LastModifier", "CreateAt", "UpdatedAt")
 
-		if len(paramOutForCounting) == 0 {
-			return response.Failure(util.ErrorFieldsToBeCreatedNotFound)
+		//再增加新的记录
+		var paramOut []model.UserAndRole
+		for _, roleID := range r.RoleIDs {
+			var record model.UserAndRole
+			if r.Creator > 0 {
+				record.Creator = &r.Creator
+			}
+			if r.LastModifier > 0 {
+				record.LastModifier = &r.LastModifier
+			}
+
+			record.UserID = r.UserID
+			record.RoleID = roleID
+			paramOut = append(paramOut, record)
 		}
-	}
 
-	err = global.DB.Create(&paramOut).Error
-	if err != nil {
-		global.SugaredLogger.Errorln(err)
+		for i := range paramOut {
+			//计算有修改值的字段数，分别进行不同处理
+			tempParamOut, err := util.StructToMap(paramOut[i])
+			if err != nil {
+				return ErrorFailToUpdateRecord
+			}
+			paramOutForCounting := util.MapCopy(tempParamOut,
+				"Creator", "LastModifier", "CreateAt", "UpdatedAt")
+
+			if len(paramOutForCounting) == 0 {
+				return ErrorFieldsToBeCreatedNotFound
+			}
+		}
+
+		err = global.DB.Create(&paramOut).Error
+		if err != nil {
+			global.SugaredLogger.Errorln(err)
+			return ErrorFailToCreateRecord
+		}
+
+		//更新casbin的rbac分组规则
+		var param1 rbacUpdateGroupingPolicyByMember
+		param1.Member = strconv.Itoa(r.UserID)
+		for _, roleID := range r.RoleIDs {
+			param1.Groups = append(param1.Groups, strconv.Itoa(roleID))
+		}
+		err = param1.Update()
+		if err != nil {
+			return ErrorFailToUpdateRBACGroupingPolicies
+		}
+
+		return nil
+	})
+
+	switch err {
+	case nil:
+		return response.Success()
+	case ErrorFailToCreateRecord:
+		return response.Failure(util.ErrorFailToCreateRecord)
+	case ErrorFailToDeleteRecord:
+		return response.Failure(util.ErrorFailToDeleteRecord)
+	case ErrorFieldsToBeCreatedNotFound:
+		return response.Failure(util.ErrorFieldsToBeCreatedNotFound)
+	case ErrorFailToUpdateRBACGroupingPolicies:
+		return response.Failure(util.ErrorFailToUpdateRBACGroupingPolicies)
+	default:
 		return response.Failure(util.ErrorFailToUpdateRecord)
 	}
-
-	//更新casbin的rbac分组规则
-	var param1 rbacUpdateGroupingPolicyBySon
-	param1.Son = strconv.Itoa(r2.UserID)
-	for _, roleID := range r2.RoleIDs {
-		param1.Fathers = append(param1.Fathers, strconv.Itoa(roleID))
-	}
-	err = param1.UpdateGroupingPolicyBySon()
-	if err != nil {
-		return response.Failure(util.ErrorFailToUpdateRBACGroupingPolicies)
-	}
-	return response.Success()
 }
